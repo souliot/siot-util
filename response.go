4b3640f25aaa8d9031bd46cffd3d254a3331b008
souliot <linleizhou19910924@163.com>
2020-03-19 16:53:55 +0800

    fix(response): update err struct

--- response.go
+++ response.go
@@ -13,18 +13,36 @@ type ControllerSuccess struct {
 	Data   interface{} `json:"data"`
 }
 
+// define Col operations
+const (
+	TypeErrSystem   = "服务器错误"
+	TypeErrDatabase = "数据库错误"
+	TypeErrMq       = "消息服务错误"
+	TypeErrCache    = "缓存服务错误"
+	TypeErrUser     = "用户操作错误"
+)
+
+const (
+	StatusSuccess         = 200
+	StatusTypeErrSystem   = 500
+	StatusTypeErrDatabase = 600
+	StatusTypeErrMq       = 700
+	StatusTypeErrCache    = 800
+	StatusTypeErrUser     = 400
+)
+
 var (
-	Err404       = &ControllerError{404, 404, "page not found", "page not found", ""}
+	Err404       = &ControllerError{StatusTypeErrUser, 404, "page not found", "page not found", ""}
 	ErrInputData = &ControllerError{400, 10001, "数据输入错误", "客户端参数错误", ""}
 
 	ErrTransferData = &ControllerError{500, 20001, "数据转换错误", "Json 字符串转 Map 错误", ""}
 
-	ErrDatabase     = &ControllerError{400, 10002, "数据库错误", "数据库操作错误", ""}
-	ErrDupRecord    = &ControllerError{400, 10003, "数据库错误", "数据库记录重复", ""}
-	ErrNoRecord     = &ControllerError{400, 10004, "数据库错误", "数据库记录不存在", ""}
-	ErrPass         = &ControllerError{400, 10005, "数据库错误", "密码不正确", ""}
-	ErrNoUserPass   = &ControllerError{400, 10006, "数据库错误", "数据库记录不存在或密码不正确", ""}
-	ErrNoUserChange = &ControllerError{400, 10007, "数据库错误", "数据库记录不存在或数据未改变", ""}
+	ErrDatabase     = &ControllerError{StatusTypeErrDatabase, 10002, TypeErrDatabase, "数据库操作错误", ""}
+	ErrDupRecord    = &ControllerError{StatusTypeErrDatabase, 10003, TypeErrDatabase, "数据库记录重复", ""}
+	ErrNoRecord     = &ControllerError{StatusTypeErrDatabase, 10004, TypeErrDatabase, "数据库记录不存在", ""}
+	ErrPass         = &ControllerError{StatusTypeErrDatabase, 10005, TypeErrDatabase, "密码不正确", ""}
+	ErrNoUserPass   = &ControllerError{StatusTypeErrDatabase, 10006, TypeErrDatabase, "数据库记录不存在或密码不正确", ""}
+	ErrNoUserChange = &ControllerError{StatusTypeErrDatabase, 10007, TypeErrDatabase, "数据库记录不存在或数据未改变", ""}
 
 	ErrInvalidUser = &ControllerError{400, 10008, "用户信息不正确", "Session信息不正确", ""}
 
