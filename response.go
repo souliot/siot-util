f921ce0e47decfc2dead80fdc81ea6451dbb58c7
souliot <linleizhou19910924@163.com>
2020-03-19 19:15:26 +0800

    fix(reqponse): update status code

--- response.go
+++ response.go
@@ -32,26 +32,31 @@ const (
 )
 
 var (
-	Err404       = &ControllerError{StatusTypeErrUser, 404, "page not found", "page not found", ""}
-	ErrInputData = &ControllerError{400, 10001, "数据输入错误", "客户端参数错误", ""}
-
-	ErrTransferData = &ControllerError{500, 20001, "数据转换错误", "Json 字符串转 Map 错误", ""}
-
-	ErrDatabase     = &ControllerError{StatusTypeErrDatabase, 10002, TypeErrDatabase, "数据库操作错误", ""}
-	ErrDupRecord    = &ControllerError{StatusTypeErrDatabase, 10003, TypeErrDatabase, "数据库记录重复", ""}
-	ErrNoRecord     = &ControllerError{StatusTypeErrDatabase, 10004, TypeErrDatabase, "数据库记录不存在", ""}
-	ErrPass         = &ControllerError{StatusTypeErrDatabase, 10005, TypeErrDatabase, "密码不正确", ""}
-	ErrNoUserPass   = &ControllerError{StatusTypeErrDatabase, 10006, TypeErrDatabase, "数据库记录不存在或密码不正确", ""}
-	ErrNoUserChange = &ControllerError{StatusTypeErrDatabase, 10007, TypeErrDatabase, "数据库记录不存在或数据未改变", ""}
-
-	ErrInvalidUser = &ControllerError{400, 10008, "用户信息不正确", "Session信息不正确", ""}
-
-	ErrOpenFile   = &ControllerError{500, 10009, "服务器错误", "打开文件出错", ""}
-	ErrWriteFile  = &ControllerError{500, 10010, "服务器错误", "写文件出错", ""}
-	ErrSystem     = &ControllerError{500, 10011, "服务器错误", "操作系统错误", ""}
-	ErrExpired    = &ControllerError{400, 10012, "登录已过期", "验证token过期", ""}
-	ErrPermission = &ControllerError{400, 10013, "没有权限", "没有操作权限", ""}
 	Actionsuccess = &ControllerError{200, 90000, "操作成功", "操作成功", ""}
+)
 
-	ErrDeviceQrAdd = &ControllerError{400, 20000, "添加失败", "设备二维码无效或已被别的用户管理！", ""}
+var (
+	ErrDb                  = &ControllerError{600, 10001, "数据库错误", "数据库操作错误", ""}
+	ErrDupRecord           = &ControllerError{600, 10002, "数据库错误", "数据库记录重复", ""}
+	ErrNoRecord            = &ControllerError{600, 10003, "数据库错误", "数据库记录不存在", ""}
+	ErrUserPass            = &ControllerError{600, 10004, "数据库错误", "用户名或密码不正确", ""}
+	ErrDbInsert            = &ControllerError{600, 10005, "数据库错误", "数据添加错误", ""}
+	ErrDbUpdate            = &ControllerError{600, 10006, "数据库错误", "数据更新错误", ""}
+	ErrDbDelete            = &ControllerError{600, 10007, "数据库错误", "数据删除错误", ""}
+	ErrChangeAccountStatus = &ControllerError{600, 10008, "数据库错误", "更新账户状态错误", ""}
+)
+
+var (
+	ErrInvalidUser = &ControllerError{400, 10008, "用户信息不正确", "Session信息不正确", ""}
+	ErrOpenFile    = &ControllerError{500, 10009, "服务器错误", "打开文件出错", ""}
+	ErrWriteFile   = &ControllerError{500, 10010, "服务器错误", "写文件出错", ""}
+	ErrSystem      = &ControllerError{500, 10011, "服务器错误", "操作系统错误", ""}
+	ErrExpired     = &ControllerError{400, 10012, "登录已过期", "验证token过期", ""}
+	ErrPermission  = &ControllerError{400, 10013, "没有权限", "没有操作权限", ""}
+)
+
+var (
+	Err404          = &ControllerError{StatusTypeErrUser, 404, "page not found", "page not found", ""}
+	ErrInputData    = &ControllerError{400, 10001, "数据输入错误", "客户端参数错误", ""}
+	ErrTransferData = &ControllerError{500, 20001, "数据转换错误", "Json 字符串转 Map 错误", ""}
 )
